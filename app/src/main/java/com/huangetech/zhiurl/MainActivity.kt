0140a6c438d3a307da88eee383d24a58be2c2315
muzihuaner <muzihuaner@hotmail.com>
2022-03-05 20:09:33 +0800

    Initial commit

---
+++ app/src/main/java/com/huangetech/zhiurl/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.huangetech.zhiurl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONArray
+import java.io.BufferedReader
+import java.io.DataOutputStream
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.lang.StringBuilder
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        shorturl.setOnClickListener {
+            sendRequestWithHttpURLConnection()
+        }
+    }
+
+
+    private fun sendRequestWithHttpURLConnection() {
+//        开启线程发起网络请求
+        thread {
+            var connection: HttpURLConnection? = null
+            try {
+                var geturl = inputurl.text.toString()
+                var response = StringBuilder()
+                val url = URL("https://zhiurl.cn")
+                connection = url.openConnection() as HttpURLConnection
+                connection.connectTimeout = 8000
+                connection.readTimeout = 8000
+                connection.requestMethod = "POST"
+                connection.setRequestProperty("Content-Type", "application/json")
+                val output = DataOutputStream(connection.outputStream)
+                output.writeBytes("{\"url\":\"$geturl\"}")
+
+                val input = connection.inputStream
+//                对获取到的输入流进行读取
+                val reader = BufferedReader(InputStreamReader(input))
+                reader.use {
+                    reader.forEachLine {
+                        response.append(it)
+                    }
+                }
+
+                if (response!=null){
+                    parseJSONWithObject(response.toString())
+                }
+//                showResponse(response.toString())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                connection?.disconnect()
+            }
+        }
+    }
+
+    private fun parseJSONWithObject(jsonData: String) {
+        try {
+            val jsonArray= JSONArray(jsonData)
+            for (i in 0 until jsonArray.length()){
+                val jsonObject=jsonArray.getJSONObject(i)
+                val static=jsonObject.getString("status")
+                val getshorturl="https://zhiurl.cn"+jsonObject.getString("key")
+                Log.d("MainActivity", "status is $static")
+                Log.d("MainActivity", "shorturl is $getshorturl")
+            }
+
+        }catch (e:Exception){
+            e.printStackTrace()
+        }
+    }
+
+    private fun showResponse(response: String) {
+        runOnUiThread {
+//            在这里进行UI操作，将结果显示在界面上
+            ResponseText.text = response
+        }
+    }
+}
